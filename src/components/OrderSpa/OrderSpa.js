18706bd10eb8f99d9dc08ee08b7fe5ba57d04950
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-07 09:42:47 -0500

    added components boilerplate

---
+++ src/components/OrderSpa/OrderSpa.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './OrderSpa.css';
+
+class OrderSpa extends React.Component {
+  render () {
+    return (
+      <div className="OrderSpa">
+        <h1>Order Spa</h1>
+      </div>
+    );
+  }
+}
+
+export default OrderSpa;
---
+++ src/components/SingleOrder/SingleOrder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './SingleOrder.css';
+
+class SingleOrder extends React.Component {
+  render () {
+    return (
+      <div className="singleOrder">
+        <h1>Single Order</h1>
+      </div>
+    );
+  }
+}
+
+export default SingleOrder;
