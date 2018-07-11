2a4b40125a6bab3947c2a1760747c6a3b2338ac7
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-10 20:30:39 -0500

    added new code that prints out framework for single ordr page

--- src/components/SingleOrder/SingleOrder.js
+++ src/components/SingleOrder/SingleOrder.js
@@ -1,5 +1,10 @@
 import React from 'react';
+// import moment from 'moment';
+
 import orderRequests from '../../FirebaseRequests/orders';
+// import fishRequests from '../../firebaseRequests/fishes';
+
+// import formatPrice from '../../helpers';
 
 import './SingleOrder.css';
 
@@ -12,16 +17,40 @@ class SingleOrder extends React.Component {
       .then(() => {
         this.props.history.push('/orders');
       })
-      .catch((err) => {
-        console.error('error while deleteing Order', err);
-      });
-  };
+      .catch(((err) => {
+        console.error('error with get delete request', err);
+      }));
+  }
 
   render () {
     return (
-      <div className="singleOrder">
-        <h1>Single Order</h1>
-        <button className="btn btn-danger" onClick={this.deleteOrderClick}>Delete Order</button>
+      <div className="SingleOrder col-xs-12 text-center">
+        <h2>Order Number: </h2>
+        <h4>Order Date:</h4>
+        <div className="row fishes">
+          <div className="col-xs-8 col-xs-offset-2">
+            {/* <ul>{fishComponents}</ul> */}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-8 col-xs-offset-2 totals">
+            <h3>
+              Total Cost: <strong></strong>
+            </h3>
+          </div>
+        </div>
+        <div>
+          <div className="col-xs-6">
+            <button className="col-xs-12 btn btn-default">
+              Update Order
+            </button>
+          </div>
+          <div className="col-xs-6">
+            <button className="col-xs-12 btn btn-danger" onClick={this.deleteOrderClick}>
+              Delete Order
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
