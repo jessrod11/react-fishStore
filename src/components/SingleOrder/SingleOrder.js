554dd4ad0dab340b201ac62ddc2fd53aa797dcf2
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-10 20:27:07 -0500

    ability to delete and send back to orders page

--- src/components/SingleOrder/SingleOrder.js
+++ src/components/SingleOrder/SingleOrder.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import orderRequests from '../../FirebaseRequests/orders';
 
 import './SingleOrder.css';
 
 class SingleOrder extends React.Component {
+
+  deleteOrderClick = () => {
+    const firebaseId = this.props.match.params.id;
+    orderRequests
+      .deleteRequest(firebaseId)
+      .then(() => {
+        this.props.history.push('/orders');
+      })
+      .catch((err) => {
+        console.error('error while deleteing Order', err);
+      });
+  };
+
   render () {
     return (
       <div className="singleOrder">
         <h1>Single Order</h1>
+        <button className="btn btn-danger" onClick={this.deleteOrderClick}>Delete Order</button>
       </div>
     );
   }
