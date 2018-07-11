8e08db8f558c7bb4828521f9eba5df50c8a5ed96
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-10 19:02:48 -0500

    total price printing

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -23,15 +23,26 @@ class Order extends React.Component {
       </li>
     );
   }
+
   render () {
     const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes.find(x => x.id === key);
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === 'available';
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+
     return (
       <div className="Order">
         <h2>Order</h2>
         <ul>
           {orderIds.map(this.renderOrder)}
         </ul>
-        <div className="total">Total:</div>
+        <div className="total">Total:<strong>{formatPrice(total)}</strong></div>
         <button className="btn btn-default">Save Order</button>
       </div>
     );
